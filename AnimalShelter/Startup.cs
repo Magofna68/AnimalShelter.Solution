d8b140fce6301e7bf9da889b822c4ce5994ae326
Brandon-Magofna <magofna68@gmail.com>
2021-06-11 10:12:34 -0700

    :bug: fix; successful builds

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -3,9 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using AnimalShelter.Models;
 using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
+using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
 namespace AnimalShelter
