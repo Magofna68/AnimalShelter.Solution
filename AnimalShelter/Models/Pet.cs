782638a7436737e38eac8d2777e26231e2898a04
Brandon-Magofna <magofna68@gmail.com>
2021-06-11 09:16:31 -0700

    :sparkles: build out models/pet.cs

---
+++ AnimalShelter/Models/Pet.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalShelter
+{
+  public class Pet
+  {
+    public int AnimalId { get; set; }
+    [Required]
+    [StringLength(3)]
+    public string Type { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Gender { get; set; }
+    public string Breed { get; set; }
+    [Required]
+    public int Age { get; set; }
+    [Required]
+    public string Description { get; set; }
+  }
+}
